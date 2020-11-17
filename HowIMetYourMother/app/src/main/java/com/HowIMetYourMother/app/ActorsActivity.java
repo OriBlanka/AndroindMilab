3139ed9af7fc3f13845da3c5ff4d6840b6c0f7f3
OriBlanka <oriblanka305@gmail.com>
2020-11-17 15:11:28 +0200

    Add recycler view and its adapter

--- HowIMetYourMother/app/src/main/java/com/HowIMetYourMother/app/ActorsActivity.java
+++ HowIMetYourMother/app/src/main/java/com/HowIMetYourMother/app/ActorsActivity.java
@@ -1,6 +1,8 @@
 package com.HowIMetYourMother.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,8 @@ public class ActorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actors);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.actorsList);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
     }
 }
--- HowIMetYourMother/app/src/main/java/com/HowIMetYourMother/app/MainActivity.java
+++ HowIMetYourMother/app/src/main/java/com/HowIMetYourMother/app/MainActivity.java
@@ -2,14 +2,35 @@ package com.HowIMetYourMother.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button actorButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actorButton = (Button)findViewById(R.id.actorsButton);
+        actorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent actorsIntent = new Intent(v.getContext(), ActorsActivity.class);
+                startActivity(actorsIntent);
+                //openActorsActivity();
+            }
+        });
     }
+
+    public void openActorsActivity(){
+        Intent actorsIntent = new Intent(this.getApplicationContext(), ActorsActivity.class);
+        startActivity(actorsIntent);
+    }
+
+
 
 }
