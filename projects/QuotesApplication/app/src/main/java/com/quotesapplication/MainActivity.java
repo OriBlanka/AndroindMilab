1a8563466b3ed9df0134c7782399001fb6feb664
OriBlanka <37545253+OriBlanka@users.noreply.github.com>
2020-12-04 16:12:50 +0200

    Adding button that show a notification

--- projects/QuotesApplication/app/src/main/java/com/quotesapplication/MainActivity.java
+++ projects/QuotesApplication/app/src/main/java/com/quotesapplication/MainActivity.java
@@ -1,14 +1,57 @@
 package com.quotesapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btStartNotification;
+    static String CHANNEL_ID = "CHANNEL";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationChannel();
+
+        btStartNotification = (Button)findViewById(R.id.quotesButton);
+        btStartNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = "This is a notification";
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(view.getContext(), CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_verified)
+                        .setContentTitle("Smart Quotes")
+                        .setContentText(message);
+
+                NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+                int notifyId = 1;
+                notificationManager.notify(notifyId, builder.build());
+            }
+        });
+    }
+
+    private void createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Ex3_channel";
+            //String description = "Just some description";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            //channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+
     }
 }
