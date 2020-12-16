82f40ff89e7da4fbc54857f2fd4eaf6290bee70e
OriBlanka <37545253+OriBlanka@users.noreply.github.com>
2020-12-16 19:11:41 +0200

    trying the example from the lecture

--- Exercise4/app.js
+++ Exercise4/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 let server=http.createServer(function(request,response) {
     response.writeHead(200);
-    response.end("Hello World!\n");
+    response.end("Hello EveryOne!\n");
 });
 
 server.listen(8080);
