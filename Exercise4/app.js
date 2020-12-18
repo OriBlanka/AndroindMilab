9bb5553245c5c02a0eda97dfc428692ea27ffd5a
OriBlanka <37545253+OriBlanka@users.noreply.github.com>
2020-12-18 17:44:39 +0200

    Adding remove and remove all function

--- Exercise4/app.js
+++ Exercise4/app.js
@@ -4,23 +4,25 @@ const fs = require('fs');
 let app = express();
 app.use(bodyParser.json());
 
-
+//See all tasks in the TODO list
 app.get('/tasks', (req, res) => {
     res.send(readJsonFile());
     console.log("tasks.js is displayed");
 });
 
+//Add a new task to the TODO list
 app.get('/tasks/new',(req, res) => {
     
     let tasksList = JSON.parse(readJsonFile());
     let id = req.query.id || "NaN";
     let taskDescription = req.query.task || "None";
 
-    if(isNaN(id)){
+    if(isNaN(id)){ //There is no ID number
         res.send("Invalid ID number");
     } else {
-        if(!tasksList.hasOwnProperty(id)){
-            tasksList[id] = taskDescription;
+        if(!tasksList.hasOwnProperty(id)){ //There is no task with the same ID in the JSON file
+            tasksList[id] = taskDescription; 
+            //Adding the new task in the JSON file
             writeToJson(tasksList);
     
             console.log("Added new task number " + id + " to your TO DO list");
@@ -31,12 +33,48 @@ app.get('/tasks/new',(req, res) => {
     }
 });
 
+//Delete task from the TODO list
+app.get('/tasks/remove',(req, res) => {
+    
+    let tasksList = JSON.parse(readJsonFile());
+    let id = req.query.id;
+
+    if(isNaN(id)){ //There is no ID number
+        res.send("Invalid ID number");
+    } else {
+        if(tasksList.hasOwnProperty(id)){ //There is a task with the same ID in the JSON file
+            //Delete the task from the String
+            delete tasksList[id]; 
+
+            console.log("Delete task number " + id + " from your TO DO list");
+            res.send("Delete the task with id: " + id + " from JSON file");
+
+            //Update the JSON file after the deleting
+            writeToJson(tasksList);
+        } else {
+            res.send("There is no task with such ID, please try again");
+        }
+    }
+});
+
+//Delete all tasks from the TODO list
+app.get('/tasks/removeAll',(req, res) => {
+    let tasksList = { };
+
+    console.log("Delete all tasks from your TO DO list");
+    res.send("Delete all tasks from JSON file");
+
+    //Update the JSON file after the deleting
+    writeToJson(tasksList);
+});
+
 app.listen(3000,() => {
   console.log('Listening on port 3000!')});
 
-/**
- * 
- */
+/***********************************************************************************************
+ * Read the JSON file using readFileSync, abd return the file content that read before.
+ * If there is any problem, throw error message.
+ ***********************************************************************************************/
 function readJsonFile() {
     let jsonFile = fs.readFileSync('tasks.json', 'utf-8', (err) => {
         if (err){
@@ -47,14 +85,16 @@ function readJsonFile() {
     return jsonFile;
 }
 
-/**
- * 
- */
+/***********************************************************************************************
+ * The function gets JS object and create a String object from it,
+ * and writes it in the JSON file. If there is any problem, throw error message.
+ ***********************************************************************************************/
 function writeToJson(file){
     let jsonString = JSON.stringify(file, null, 4);
     fs.writeFile('tasks.json', jsonString, (err) => {
         if (err) {
             console.log("Error writing file", err);
+            throw err;
         } else {
             console.log("Successfully wrote file");
         }
