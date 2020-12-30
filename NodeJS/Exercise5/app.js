f55821022b864df7b21bb9b053c95e3ff8a525d7
OriBlanka <37545253+OriBlanka@users.noreply.github.com>
2020-12-30 20:04:49 +0200

    static file with express server

--- NodeJS/Exercise5/app.js
+++ NodeJS/Exercise5/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 let app = express();
-app.use(express.static('./public'));
+app.use(express.static('./images'));
 let path = require('path');
 
 app.get('/files/:name', (req,res) => {
@@ -11,7 +11,9 @@ app.get('/files/:name', (req,res) => {
         return; //If there is a problem with the file name, there isn't reason to continue
     }
 
-    let filePath = path.join('./public/', fileName);
+    //Create full path for the file
+    let filePath = path.join('./images/', fileName);
+
     //Check if this file exists in images folder
     if(!(fs.existsSync(filePath))){ 
         res.send("There isn't such file - please try again");
