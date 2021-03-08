5dd8ebf1a79929bf437e5ff9402e22e64118d3e3
OriBlanka <37545253+OriBlanka@users.noreply.github.com>
2021-03-08 22:19:04 +0200

    adding tab layout that includes 3 tabs - radius, view and add activity

---
+++ Test/app/src/main/java/com/tablayouttest/GroupAdp.java
@@ -0,0 +1,44 @@
+package com.tablayouttest;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class GroupAdp extends RecyclerView.Adapter<GroupAdp.ViewHolder> {
+
+    private Activity activity;
+    ArrayList<String> arrayListGroup;
+
+    GroupAdp(Activity activity, ArrayList<String> arrayListGroup){
+        this.activity = activity;
+        this.arrayListGroup = arrayListGroup;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_row_group, parent, false);
+        return view;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
